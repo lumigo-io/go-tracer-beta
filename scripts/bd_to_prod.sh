cbbf120da11686320022167f588e0b5f1e72d23c
Spiros Economakis <spiros.oikonomakis@gmail.com>
2022-02-13 17:07:39 +0200

    minor: fix release script

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -6,7 +6,7 @@ go get -u github.com/stevenmatthewt/semantics
 TAG=$(semantics --output-tag)
 if [[ ! -z "$TAG" ]] ; then
   git log --oneline > changelog.md
-  gh release create $TAG -F changelog.
+  gh release create $TAG -F changelog.md
 else 
   echo "The commit message is not major/minor/patch version"
 fi
