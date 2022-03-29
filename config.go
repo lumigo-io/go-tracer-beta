9ce8607ee318269210cdc48b55aca31128844a9c
nirLumigo <nir@lumigo.io>
2022-03-29 16:47:58 +0300

    check integration-tests

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package lumigotracer
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
@@ -26,9 +28,12 @@ var cfg Config
 // validate runs a validation to the required fields
 // for this Config struct
 func (cfg Config) validate() error { // nolint
+	fmt.Println("In cfg.validate")
 	if cfg.Token == "" {
+		fmt.Println("Validte failed")
 		return ErrInvalidToken
 	}
+	fmt.Println("Validate succeeded")
 	return nil
 }
 
@@ -44,11 +49,16 @@ func loadConfig(conf Config) error {
 	defer recoverWithLogs()
 
 	cfg.Token = viper.GetString("Token")
+	fmt.Println("cfg.Token:", cfg.Token)
 	if cfg.Token == "" {
 		cfg.Token = conf.Token
+		fmt.Println("cfg.Token in if:", cfg.Token)
 	}
 	cfg.enabled = viper.GetBool("Enabled")
+	fmt.Println("cfg.enabled:", cfg.enabled)
 	cfg.debug = viper.GetBool("Debug")
+	fmt.Println("cfg.debug:", cfg.debug)
 	cfg.PrintStdout = conf.PrintStdout
+	fmt.Println("cfg.PrintStdout:", cfg.PrintStdout)
 	return cfg.validate()
 }
